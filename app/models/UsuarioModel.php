3af13a16362f7b356838ec15f9ddc4a4c37c22f6
PEDRO MAIA FONTES LAGE <1051890@senaifiemg.com>
2026-08-24 12:08:25 -0300

    pequenas alteracoes

--- app/models/UsuarioModel.php
+++ app/models/UsuarioModel.php
@@ -2,7 +2,7 @@
 /**
  * Model para Autenticação e Controle de Acesso
  */
-require_once __DIR__ . '/Database.php';
+require_once __DIR__ . '/../config/database.php';
 
 class UsuarioModel {
     private $db;
